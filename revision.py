4c1ae2143032ecf8f7a66a0132b7d1a7bf9290ff
Daymer Vargas <daymer.vargas@ucr.ac.cr>
2022-11-06 23:48:13 -0600

    Modificaciones IMPORTANTES (HACER UN PULL ANTES DE TRABAJAR) a la funcion que obtiene el dataFrame (hice un datagrame mas facil de analizar), ademas se implementa una funcion retorna los datos c, log y scale de la distribucion de las horas de todo un ano, ademas se obtiene el polinomio de orden 7 para cada uno de estos parametros (c,log,scale), ademas cambie las fechas de inicio y final de obtencion de los datos

--- revision.py
+++ revision.py
@@ -28,7 +28,7 @@ def datos_demanda(inicio, fin):
         URL de la información para el proyecto
     datos_demanda_df    : .jason
         Archivo de .json con la informacion solicitada.
-    ==========  
+    ==========
     Returns
     ----------
     datos_demanda_df : data frame
@@ -38,14 +38,79 @@ def datos_demanda(inicio, fin):
     api_url = f'https://apps.grupoice.com/CenceWeb/data/sen/json/DemandaMW?inicio={inicio}&fin={fin}'    #Modificamos la URL.
     print('Se extraen los datos para:\nInicio:',inicio,'Fin:',fin)      #Imprimimos la direccion URL.
 
-    datos_demanda_df = requests.get(api_url).json()                     #Variable para los datos extraídos del URL.
-    datos_demanda_df = pd.DataFrame(datos_demanda_df['data'])           #Generamos el Dataframe de los datos.
+    datos_demanda_jason = requests.get(api_url).json()                     #Variable para los datos extraídos del URL.
+    datos_demanda_df = pd.DataFrame(datos_demanda_jason['data'])           #Generamos el Dataframe de los datos.
+    dayDemanda_datos_df = pd.DataFrame(datos_demanda_jason['data'])
     datos_demanda_df['fechaHora'] = pd.to_datetime(datos_demanda_df['fechaHora'])
     datos_demanda_df = datos_demanda_df.sort_values(by='fechaHora')
     datos_demanda_df.to_csv('Datos_Demanda.csv', index=False)           #Almacenamos los resultados en un archivo .csv.
 
     print('--------Data Frame generado con exito.--------')
-    return datos_demanda_df                                             #Se retornar los datos en un data frame.
+    return datos_demanda_df ,dayDemanda_datos_df                                      #Se retornar los datos en un data frame.
+
+#------Función para obtener los datos por horas:--------
+def parametros_horas():
+    '''Función distribucion_horas().
+
+    Esta función encarga de retornar los parametros de la distribucion
+    genlogistic a todas las horas del dia, a partir de DataFrame
+    de la función datos_demanda(), extrae específicamente la potencia a
+    a la hora específica, para obtener los parametros c,log,scale y retornarlos.
+
+    Parameters
+    ----------
+    hora : int
+        Variable con la hora deseada.
+    datos_hora : array
+        Contiene los datos de consumo a una hora especifica.
+    modelo : string
+        Almacena el nombre de la distribucion utilizada.
+    dayDemanda_datos_df : DataFrame
+        Una variable global que contiene los datos anuales de potencia.
+    mejorAjuste_Horas : fitter
+        Contiene información de los parametros del modelo.
+    param : dicccionario
+        contiene los parametros c, log, scale a utiliar.
+
+    Returns
+    -------
+    c : array
+        Variable con los parametros de la distribucion para c.
+    log : array
+        Variable con los parametros de la distribucion para log.
+    scale : array
+        Variable con los parametros de la distribucion para scale.
+    '''
+    #Arreglos para almacenar los datos de las distribuciones.
+    c = []
+    log = []
+    scale = []
+
+    #Ciclo para obtener la informacion y parametros de todas las horas.
+    for hora in range(0,24):
+        datos_hora = []
+        # Ciclo para recorrer todo el dataFrame en busqueda de las hora especificada
+        for i in range(hora, len(dayDemanda_datos_df.index), 24):
+            # El dato de una hora especifica aparecerá cada 24 filas, por las 24 horas del días.
+            # Almacena la potencia en la hora especifica
+            datos_hora.append(float(dayDemanda_datos_df.MW[i]))
+
+        #Seleccion del modelo.
+        modelo = 'genlogistic'
+
+        # Parametros para la hora especifica.
+        mejorAjuste_Horas = Fitter(datos_hora,distributions=[modelo])
+        mejorAjuste_Horas.fit()
+
+        #Parametros de la distribucion.
+        params = mejorAjuste_Horas.fitted_param
+
+        #Guardar los datosen los arreglos, su posicion indica su hora.
+        c.append(params[modelo][0])
+        log.append(params[modelo][1])
+        scale.append(params[modelo][2])
+
+    return c , log , scale
 #----------------------------------------------------------
 #==========================================================
 #=================Ejecución del programa:==================
@@ -57,13 +122,36 @@ fecha_inicio = input('Fecha inicio (YYYYMMDD):)                         #Solicit
 fecha_fin = input('Fecha final  (YYYYMMDD):)                            #Solicitamos la fecha de fin.
 '''
 #------------------Variables de inicio---------------------
-fecha_inicio = f'20200101'                                              #Fecha de incio para solicitud de datos.
-fecha_fin = f'20200102'                                                 #Fecha final para solicitud de datos.
+fecha_inicio = f'20190101'                                              #Fecha de incio para solicitud de datos.
+fecha_fin = f'20200101'                                                 #Fecha final para solicitud de datos.
 
 #----------------------------------------------------------
 #===========Se almacenan los datos para analizar===========
-datos_demanda_df = datos_demanda(fecha_inicio , fecha_fin)               #Llamada a la funcion datos_demanda. Generamos el df.
+datos_demanda_df,dayDemanda_datos_df = datos_demanda(fecha_inicio , fecha_fin)               #Llamada a la funcion datos_demanda. Generamos el df.
 DF_demanda_hora = pd.read_csv('Datos_Demanda.csv')                       #Caragamos el Data Frame generado por datos_demanda.
+#----------------------------------------------------------
+
+#print (DF_demanda_hora)
+
+#----------------------------------------------------------
+#===========Obtencion del los parametros por horas===========
+c,log,scale = parametros_horas()
+x = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
+
+#----------------------------------------------------------
+#===========Encontrar c(t)===========
+c_t = np.polyfit(x,c,7)
+print(c_t)
+
+#----------------------------------------------------------
+#===========Encontrar log(t)===========
+log_t = np.polyfit(x,log,7)
+print(log_t)
+
+#----------------------------------------------------------
+#===========Encontrar scale(t)===========
+scale_t = np.polyfit(x,scale,7)
+print(scale_t)
 #----------------------------------------------------------
 
 # SECCIÓN A: Función de densidad de probabilidad
@@ -108,7 +196,7 @@ print(C7)
 C8 = estacionaridad.ergodicidad()
 print(C8)
 
-# SECCIÓN D: Características espectrales 
+# SECCIÓN D: Características espectrales
 
 # 9. Función de densidad espectral de potencia
 D9 = espectro.psd()
