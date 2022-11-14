14e9fa067f16a9f6ba0a5eb1ceba21f65dc2e6e2
ARGCANT <112878524+ARGCANT@users.noreply.github.com>
2022-11-14 15:45:16 -0600

    Cambios efectuados
    Se implementó la función covarianza.

--- proceso/momentos.py
+++ proceso/momentos.py
@@ -29,5 +29,26 @@ correlacion = autocorrelacion()
 print("El coeficiente obtenido de correlación de Pearson entre las horas "
 + str(horas[0]) + ":00 y " + str(horas[1]) + ":00 es de: \n", correlacion[0])
 
+# Se define la función de autocovarianza
+
 def autocovarianza():
-    return 'Aquí está la autocovarianza.'
+    
+# Se consideran los datos de la primera hora asignada 
+periodo1 = datos_hora(horas[0]).assign(Index =
+range (len (datos_hora(horas[0])))).set_index('Index')['MW'] 
+
+# Se consideran los datos de la segunda hora asignada
+periodo2 = datos_hora(horas[1]).assign(Index =
+range (len (datos_hora(horas[1])))).set_index('Index')['MW']
+
+# Es necesario guardar en una serie los dos periodos
+consumoP1 = pd.Series(data=periodo1) 
+consumoP2 = pd.Series(data=periodo2) 
+
+# Se calcula la covarianza mediante el uso de pandas
+covarianza= consumoP1.cov(consumoP2)
+return covarianza
+
+covarianza= autocovarianza()
+
+print ("La covarianza obtenida entre la hora establecida " + str(horas[0]) + ":00 y la hora " + str(horas[1]) + ":00 es de " + str(covarianza))
