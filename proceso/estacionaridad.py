3cc3d425b3657f8ab8b5bd7a90ac0c15fea5752e
Soto <jafets.a22@gmail.com>
2022-11-14 21:32:25 -0600

    Actualización del archivo estacionaridad.py unicamente falta la ultima funcion.

--- proceso/estacionaridad.py
+++ proceso/estacionaridad.py
@@ -1,15 +1,14 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from statsmodels.tsa.stattools import adfuller
 
-def wss(DF):
+def wss(A0):
     print("============Modulo Estacionaridad :=============")
     print("=======Estacionaridad en sentido amplio:========")
     print("----------Resultados de Dickey-fuller:----------")
-    DF_DICKEYFULLER = adfuller(DF['MW'], autolag='AIC')
+    DF_DICKEYFULLER = adfuller(A0[0], autolag='AIC')
     DF_SALIDA = pd.Series(DF_DICKEYFULLER[0:4], index = ['Test Statistic','p-value','#lags used','number of observations used'])
-    
-    
     for key, value in DF_DICKEYFULLER[4].items():
         DF_SALIDA['critical value (%s)'%key]= value
     
@@ -19,8 +18,9 @@ def wss(DF):
         print('Serie temporal estacionaria en setido amplio.')
     return '------------------------------------------------'
 
-def prom_temporal():
-    return 'Aquí está el promedio temporal.'
+def prom_temporal(A0):
+    PROMEDIO_TEMPORAL = np.convolve(A0[0], np.ones(8), 'valid')/8
+    return float(PROMEDIO_TEMPORAL)
 
-def ergodicidad():
+def ergodicidad(A0):
     return 'Aquí está la ergodicidad.'
