68d977147596b20ee0a0aa15d7e40e29982de194
Soto <jafets.a22@gmail.com>
2022-11-15 01:15:03 -0600

    Ahora el archivo estacionaridad teine sus 2 funciones comentadas

--- proceso/estacionaridad.py
+++ proceso/estacionaridad.py
@@ -4,6 +4,20 @@ import matplotlib.pyplot as plt
 from statsmodels.tsa.stattools import adfuller
 
 def wss(A0):
+    '''Esta funcion determina la estacionaridad en sentido amplio
+        de una secuencia aleatorea, manteniedo los valores validos
+        en un factor del 5% de diferencia
+    ==========
+    Parameters:
+    ----------
+    A0                  : DataFrame
+        URL de la información para el proyecto.
+    DF_DICKEYFULLER     : Dta frame
+        Valores de prueba de DickeyFuller para estacionaridad.
+    DF_SALIDA           : Data frame
+        Contiene los valores finales de las pruebas.
+    ==========
+    '''
     print("============Modulo Estacionaridad :=============")
     print("=======Estacionaridad en sentido amplio:========")
     print("----------Resultados de Dickey-fuller:----------")
@@ -19,6 +33,23 @@ def wss(A0):
     return '------------------------------------------------'
 
 def prom_temporal(C,A0):
+    '''Esta funcion determina el promedio temporal para los valores
+        de consumo MW, se calcula tanto el vcalor numerico sujeto a 
+        la funcion 'C' como el vector de promedio temporal para el
+        data frame de las horas.
+    ==========
+    Parameters:
+    ----------
+    C                   : list
+        Secuencia aleatoria de consumo de MW.
+    A0                  : DataFrame
+        URL de la información para el proyecto.
+    PROMEDIO_TEMPORAL_ADD: Dta frame
+        Almacena los valores para el nuevo vector de A0.
+    PROMEDIO_TEMPORAL   : float
+        Valor numerico del promedio temporal para la funcion 'C.
+    ==========
+    '''
     print("===============Promedio temporal:===============")
     A0['fechaHora'] = pd.to_datetime(A0['fechaHora'])
     A0 = A0.sort_values(by='fechaHora')
