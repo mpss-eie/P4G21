669f2675215f8254c5f5f8302481be7870d6b26c
Daymer Vargas <daymer.vargas@ucr.ac.cr>
2022-11-16 20:22:55 -0600

    Funcion de espectro funcionando

--- proceso/espectro.py
+++ proceso/espectro.py
@@ -1,7 +1,7 @@
  # librerías
 import json
 import requests
-import pandas as pd 
+import pandas as pd
 from pandas import json_normalize
 from fitter import Fitter
 import numpy as np
@@ -9,31 +9,28 @@ from scipy import stats
 from scipy.stats import gamma
 import matplotlib.pyplot as plt
 
+# Crea un dataframe para una hora especifica con los datos brindados
+def datos_hora(datos_df,hora):
+    # Se obtiene el consumo a la primera hora específica.
+    datos_t1 = []
+    for i in range(hora, len(datos_df.index), 24):
+        datos_t1.append(float(datos_df.MW[i]))
+
+    return datos_t1
+
 def psd(datos_df, hora):
-   
+
     # Protección contra errores
     if (hora < 0) or (hora > 23):
         error = 'El programa está trabajando con valores inválidos de tiempo.'
         return error
     else:
-        # Crea un dataframe para una hora especifica con los datos brindados
-        def datos_hora(hora):
-            if hora > 9:
-                hora = '{}:00:00.0'.format(hora)
-            else:
-                hora = '0{}:00:00.0'.format(hora)
-            consumo_pot = datos_df[datos_df['fechaHora'].str.contains(hora)]
-            del consumo_pot['MW_P']
-            return consumo_pot
         # En esta parte se crea una función muestra para una hora determinada que representa la
         # densidad espectral de potencia
-        muestra = datos_hora(hora).assign(Index=
-                        range(len(datos_hora(hora)))).set_index('Index')['MW']
+        muestra = datos_hora(datos_df,hora)
         # Se grafica la funcion muestra con 800 puntos
         plt.psd(muestra, 800, 1./0.01)
-        plt.xlabel('Frecuencia')
+        plt.xlabel('Frecuencia (Hz)')
         plt.ylabel('Densidad Espectral')
         plt.show()
         return 'Se muestra la gráfica de la densidad espectral'
-    
-    
--- revision.py
+++ revision.py
@@ -1,74 +1,10 @@
 
 from proceso import  espectro, momentos, proceso, estacionaridad
 
-# ----------Importamos las bibliotecas necesarias:----------
-import requests
-import json
-import pandas as pd
-import csv
-import numpy as np
-import matplotlib.pyplot as plt
-
-from fitter import HistFit
-from pylab import hist
-from fitter import Fitter
-from datetime import time
-from scipy import stats
-from scipy.stats import pearsonr
-# ----------------------------------------------------------
-# =================Carga de Datos diários:==================
-# ----------------------------------------------------------
-# --------Función para importar los datos del CENCE:--------
-
-
-def datos_demanda(inicio, fin):
-    '''
-    ==========
-    Parameters
-    ----------
-    api_url             : str
-        URL de la información para el proyecto
-    datos_demanda_df    : .jason
-        Archivo de .json con la informacion solicitada.
-    ==========
-    Returns
-    ----------
-    datos_demanda_df : data frame
-        Data frame de los datos solicitados en el periodo de tiempo correspondiente.
-    ==========
-    '''
-    api_url = f'https://apps.grupoice.com/CenceWeb/data/sen/json/DemandaMW?inicio={inicio}&fin={fin}'  # Modificamos la URL.
-    # Imprimimos la direccion URL.
-    print('Se extraen los datos para:\nInicio:', inicio, 'Fin:', fin)
-
-    # Variable para los datos extraídos del URL.
-    datos_demanda_jason = requests.get(api_url).json()
-    # Generamos el Dataframe de los datos.
-    datos_demanda_df = pd.DataFrame(datos_demanda_jason['data'])
-    dayDemanda_datos_df = pd.DataFrame(datos_demanda_jason['data'])
-    datos_demanda_df['fechaHora'] = pd.to_datetime(
-        datos_demanda_df['fechaHora'])
-    datos_demanda_df = datos_demanda_df.sort_values(by='fechaHora')
-    # Almacenamos los resultados en un archivo .csv.
-    datos_demanda_df.to_csv('Datos_Demanda.csv', index=False)
-
-    print('--------Data Frame generado con exito.--------')
-    # Se retornar los datos en un data frame.
-    return datos_demanda_df, dayDemanda_datos_df
-
-
-# ----------------------------------------------------------
-# ==========================================================
-# =================Ejecución del programa:==================
-''
-#print (DF_demanda_hora)
-
-
 # SECCIÓN A: Procesos
 
 # 0. Datos de demanda de potencia
 A0 = proceso.demanda()
-# print(A0)
 
 # 1. Función de densidad del proceso aleatorio
 c, log, scale = proceso.densidad(A0)
@@ -79,7 +15,7 @@ print(f"Polinomio para scale:{scale}")
 print("#########################################################")
 
 # 2. Gráfica de la secuencia aleatoria
-A2 = proceso.grafica(A0)
+#A2 = proceso.grafica(A0)
 
 # 3. Probabilidad de tener un consumo p1 < P < p2 en t1 < T < t2
 # datas a ingresar(dataframe, hora Inicial, hora Final, potencia Inicial, potencia Final)
@@ -118,8 +54,9 @@ print(C7)
 # 8. Ergodicidad
 C8 = estacionaridad.ergodicidad(A0, C71)
 print(C8)
+
 # SECCIÓN D: Características espectrales
 # 9. Función de densidad espectral de potencia
 
-D9 = espectro.psd()
+D9 = espectro.psd(A0,17)
 print(D9)
